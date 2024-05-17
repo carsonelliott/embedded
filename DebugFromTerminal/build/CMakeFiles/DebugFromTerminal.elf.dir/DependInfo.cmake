ea1c5d03fc2dad1f90a0480a789b3961cd2e94e2
carsonelliott <carsone6@gmail.com>
2024-05-17 17:08:23 -0400

    add project folder

---
+++ DebugFromTerminal/build/CMakeFiles/DebugFromTerminal.elf.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Startup/startup_stm32h723zgtx.s" "/Users/carson/Desktop/embedded/DebugFromTerminal/build/CMakeFiles/DebugFromTerminal.elf.dir/Startup/startup_stm32h723zgtx.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Inc"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Src/main.c" "CMakeFiles/DebugFromTerminal.elf.dir/Src/main.c.o" "gcc" "CMakeFiles/DebugFromTerminal.elf.dir/Src/main.c.o.d"
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Src/stm32h7xx_hal_msp.c" "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_hal_msp.c.o" "gcc" "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_hal_msp.c.o.d"
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Src/stm32h7xx_it.c" "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_it.c.o" "gcc" "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_it.c.o.d"
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Src/syscalls.c" "CMakeFiles/DebugFromTerminal.elf.dir/Src/syscalls.c.o" "gcc" "CMakeFiles/DebugFromTerminal.elf.dir/Src/syscalls.c.o.d"
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Src/sysmem.c" "CMakeFiles/DebugFromTerminal.elf.dir/Src/sysmem.c.o" "gcc" "CMakeFiles/DebugFromTerminal.elf.dir/Src/sysmem.c.o.d"
+  "/Users/carson/Desktop/embedded/DebugFromTerminal/Src/system_stm32h7xx.c" "CMakeFiles/DebugFromTerminal.elf.dir/Src/system_stm32h7xx.c.o" "gcc" "CMakeFiles/DebugFromTerminal.elf.dir/Src/system_stm32h7xx.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ DebugFromTerminal/build/CMakeFiles/DebugFromTerminal.elf.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/main.c.o"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/main.c.o.d"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_hal_msp.c.o"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_hal_msp.c.o.d"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_it.c.o"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/stm32h7xx_it.c.o.d"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/syscalls.c.o"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/syscalls.c.o.d"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/sysmem.c.o"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/sysmem.c.o.d"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/system_stm32h7xx.c.o"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Src/system_stm32h7xx.c.o.d"
+  "CMakeFiles/DebugFromTerminal.elf.dir/Startup/startup_stm32h723zgtx.s.o"
+  "DebugFromTerminal.elf"
+  "DebugFromTerminal.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/DebugFromTerminal.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
